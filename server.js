7da722dd58a652b370153c9506b4d070edc043dd
Prameela Pydi <nprameela.pydi@gmail.com>
2024-11-19 13:45:24 -0800

    setup middlewares

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
+import cors from 'cors';
 import {conn} from './db/conn.js';
 
 dotenv.config();
 conn();
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+//Middlewares
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 //Routes
 app.get('/', (req, res) => {
