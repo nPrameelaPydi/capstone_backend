eaf8cdecef1147ec39ad256854eb9c6df82b4ace
Prameela Pydi <nprameela.pydi@gmail.com>
2024-11-20 23:40:12 -0800

    add user Login route

--- routes/auth.js
+++ routes/auth.js
@@ -30,6 +30,31 @@ router.post('/register', async (req, res) => {
     }
 });
 
+/**
+ * /api/auth/login
+ * @description User Login route
+ */
+router.post('/login', async (req, res) => {
+    const {email, password} = req.body;
+    try{
+        // find the user by email
+        const user = await User.findOne({email});
+        if(!user) {
+            return res.status(404).json({message: 'User not found'});
+        }
+        //compare passwords
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Invalid credentials' });
+        }
+        res.status(200).json({message: 'Login Successful', userId: user._id});
+
+    }catch(err){
+        console.log(err);
+        res.status(500).json({ message: 'Error logging in', err });
+    }
+})
+
 
 
 export default router;
